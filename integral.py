14d5f19ccdd6cf4b506b3895b248ade10b788371
Armin-Rafael Zdroba-Fisteag <zdr240654@spengergasse.at>
2025-09-26 18:28:56 +0200

    Update to the indefint function - still returns errors - to be fixed

--- integral.py
+++ integral.py
@@ -2,11 +2,23 @@ import math
 
 # Very simple function for the indefinite integral (WIP)
 def indefint():
-    x = int(input("Input a base number (x): "))
-    n = int(input("Input an exponent number (n): "))
-    print(x, " ", n)
-    y = (x**(n+1))/(n+1) , "+ c" # Added the "+c for the indefinite integral"
-    print(y)
+    # x = int(input("Input a base number (x): "))
+    # n = int(input("Input an exponent number (n): "))
+    l = int(input("Input the amount of terms you would like to have (int): "))
+    # print(x, " ", n)
+    for i in range(l):
+        yr = []
+        x = int(input("Input a base number for this part of the term (x): "))
+        n = int(input("Input an exponent number for this term (n): "))
+        y = float((x**(n+1))/(n+1))
+        yr.append(y)
+        ys = yr[i]
+        if len(yr) > i:
+            print(ys)
+        else:
+            SyntaxWarning("You have somehow surpassed the index barrier")
+        i += 1
+    # print(y)
 
 def defint():
     print("We won't be needing a number 'x', as the definite integral is calculated based on the bounds.")
@@ -29,3 +41,9 @@ else:
 
 # Running some test functions to get myself further into this:
 
+# ha = 5
+# i = 0
+# for i in range(ha):
+#     i = i/2
+#     i += 1
+#     print(i)
